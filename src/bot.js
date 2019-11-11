3ce678cc64e92dc65c3c1dc33b29acf4f68ec141
Deturck Damien <damien.deturck@etu.u-bordeaux.fr>
2019-11-11 16:30:58 +0100

    o

--- src/bot.js
+++ src/bot.js
@@ -6,29 +6,29 @@ class Chat extends Component {
     constructor(props) {
         super(props);
         this.socket = io('localhost:3001', { jsomp: false });
-        this.socket.on("msg", (msg)=>{
-            console.log("réception socket "+msg)
+        this.socket.on("msg", (msg) => {
+            console.log("réception socket " + msg)
             this.state.messages.push(<p>{msg}</p>);
             this.setState({
                 chat:
-                <div className="ListMSG" id="chat">{this.state.messages}</div>,
-                value:"" 
+                    <div className="ListMSG" id="chat">{this.state.messages}</div>,
+                value: ""
             })
             this.scroll();
         })
         this.state = {
             value: '',
-            messages:[],
-            chat:<div className="ListMSG" id="chat"></div>
+            messages: [],
+            chat: <div className="ListMSG" id="chat"></div>
         }
     }
-    scroll(){
+    scroll() {
         document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
     }
     messageSubmit(event) {
-        console.log( this.state.value);
-        this.socket.emit("msg",this.state.value)
-        
+        console.log(this.state.value);
+        if (this.state.value !== "") this.socket.emit("msg", this.state.value)
+
         event.preventDefault();
     }
     render() {
@@ -36,9 +36,12 @@ class Chat extends Component {
             <div className="corp">
                 <p>salon de discussion test en react</p>
                 <div className="zoneChat" >
+                    {this.state.chat}
                     <form onSubmit={(this.messageSubmit.bind(this))}>
-                    <input type="text" value={this.state.value} onChange={(text) =>this.setState({value : text.target.value})} />   
-                    {this.state.chat}     
+                        <label>
+                            msg:  
+                        </label>
+                        <input type="text" value={this.state.value} onChange={(text) => this.setState({ value: text.target.value })} />
                     </form>
                 </div>
             </div>
