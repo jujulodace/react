f3826a1de02e38a43881393a718e9dbeafaab24e
Deturck Damien <damien.deturck@etu.u-bordeaux.fr>
2020-01-30 12:21:40 -0500

    route

--- src/App.js
+++ src/App.js
@@ -3,67 +3,58 @@ import './App.css';
 import Bot from './bot'
 import P4 from './p4'
 import io from 'socket.io-client';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-
-
-const page1 =  <Bot/>
-const page2 = <P4/>
-const page3 = <h1>page3</h1>;
-//const page4 = <h1>page4</h1>;
 
 
 
 class App extends Component {
   state = {
-    name : "bob"
+    name: "bob"
   }
-  constructor(){
+  constructor() {
     super();
-    this.socket = io('localhost:3001',{jsomp:false});
-    this.socket.on('update',()=>this.setState({name: "Name"}));
+    this.socket = io('localhost:3001', { jsomp: false });
+    this.socket.on('update', () => this.setState({ name: "Name" }));
   }
-  render() {  
+  render() {
     return (
       <div className="App">
-        <HelloWorld user="oui" />
         <h1>{this.state.name}</h1>
+        <Router>
+        <div>
+        <header className="App-header">
+         <Link to="/" className="menu">Home</Link>
+         <Link to="/about" className="menu">About</Link>
+         <Link to="/dashboard" className="menu">Dashboard</Link>
+        </header>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+            <Bot />
+            </Route>
+            <Route path="/about">
+            <P4 />
+            </Route>
+            <Route path="/dashboard">
+            <h1>page3</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       </div>
+      
     )
   }
 }
-class HelloWorld extends React.Component {
-  constructor(props) {
-    super(props);
-    this.socket = io('localhost:3001',{jsomp:false});
-    this.socket.on('update',()=>this.setState({name: "Name"}));
-    this.state = {
-      happy: false,
-      page: page1,
-      name : "bob"
-    }
-  }
-  changerPage(a) {
-   
-    this.setState({
-      page: a
-    });
-  }
-  update(){
-    this.socket.emit("update");
-  }
-  render() {  
-    return (
-      <div>
-        <header className="App-header">
-          <div className="menu" onClick={this.changerPage.bind(this,page1)}>page1</div>
-          <div className="menu" onClick={this.changerPage.bind(this,page2)}>page2</div>
-          <div className="menu" onClick={this.changerPage.bind(this,page3)}>page3</div>
-          <div className="menu" onClick={this.update.bind(this)}>page4</div>
-        </header>
-        {this.state.page}
-      </div>
-    );
-  }
-}
+
+
 
 export default App;
